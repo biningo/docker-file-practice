49524dc0dbf0b850d4f69bb84b0b3788ddc695a5
biningo <biningo@yeah.net>
2020-08-20 11:07:16 +0800

    rename

--- docker/redis-docker/standalone-redis/redis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REDIS_HOME="/usr/local/redis"
-docker run -d --name redis \
-  -v $PWD/redis.conf:$REDIS_HOME/etc/redis.conf \
-  -v $PWD/data:$REDIS_HOME/data \
-  -w $REDIS_HOME \
-  -p 6379:6379 \
-  redis  $REDIS_HOME/etc/redis.conf
